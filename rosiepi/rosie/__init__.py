ae2f46c7a7eac70a480d1b1ceec713e0c87af8fb
sommersoft <sommersoft@gmail.com>
2020-04-05 19:29:47 -0500

    rosie/__init__.py: add logging

--- rosiepi/rosie/__init__.py
+++ rosiepi/rosie/__init__.py
@@ -1,8 +1,17 @@
 import pathlib
 import sys
 
+import os
+import getpass
+
+from ..logger import rosiepi_logger
+
+rosiepi_logger.info(f"getpass.getuser: {getpass.getuser()}")
+rosiepi_logger.info(f"os.environ['USER']: {os.environ.get('USER')}")
+
 def find_circuitpython():
     user_home = pathlib.Path().home()
+    rosiepi_logger.info(f"user_home: {user_home}")
     cirpy_dir = user_home / "circuitpython"
     #print("user_home: {}\ncirpy_dir: {}".format(user_home, cirpy_dir))
     if not cirpy_dir.exists():
