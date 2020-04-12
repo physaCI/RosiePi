94834310a8a952bd1ee191851427ae218732c837
sommersoft <sommersoft@gmail.com>
2020-04-11 21:47:49 -0500

    rosie/__init__.py: remove debug logging; only log when circuitpython not found

--- rosiepi/rosie/__init__.py
+++ rosiepi/rosie/__init__.py
@@ -6,15 +6,12 @@ import getpass
 
 from ..logger import rosiepi_logger
 
-rosiepi_logger.info(f"getpass.getuser: {getpass.getuser()}")
-rosiepi_logger.info(f"os.environ['USER']: {os.environ.get('USER')}")
-
 def find_circuitpython():
     user_home = pathlib.Path().home()
-    rosiepi_logger.info(f"user_home: {user_home}")
     cirpy_dir = user_home / "circuitpython"
     #print("user_home: {}\ncirpy_dir: {}".format(user_home, cirpy_dir))
     if not cirpy_dir.exists():
+        rosiepi_logger.warning(f"{cirpy_dir} not found...")
         raise FileNotFoundError()
     return cirpy_dir
 
