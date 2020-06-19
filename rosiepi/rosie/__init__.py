2924a788fac654ee46ce8a2ec4aab3b71d72d6c6
sommersoft <sommersoft@gmail.com>
2020-06-18 22:53:19 -0500

    rosie/__init__.py: removed all code; no longer necessary

--- rosiepi/rosie/__init__.py
+++ rosiepi/rosie/__init__.py
@@ -1,19 +1,1 @@
 
-import logging
-import pathlib
-import sys
-
-
-rosiepi_logger = logging.getLogger(__name__)
-
-def find_circuitpython():
-    user_home = pathlib.Path().home()
-    cirpy_dir = user_home / "circuitpython"
-    #print("user_home: {}\ncirpy_dir: {}".format(user_home, cirpy_dir))
-    if not cirpy_dir.exists():
-        rosiepi_logger.warning("%s not found...", cirpy_dir)
-        raise FileNotFoundError()
-    return cirpy_dir
-
-sys.path.append(str(find_circuitpython()))
-#print(sys.path)
